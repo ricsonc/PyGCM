2b3b15870d83293a1a460624234b864879207983
CFFD <ricsoncheng@gmail.com>
2014-10-25 13:31:20 -0400

    Create step.py

---
+++ step.py
@@ -0,0 +1,9 @@
+print 'step: t = '+str(ABSTIME)
+
+celldata = True #can be replaced by a function later
+
+#for item in cell.data that is flagged
+    #create grid
+    #save as .png
+    #open in pil
+    #render in tkinter
